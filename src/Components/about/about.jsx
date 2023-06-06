6f6cd8bd2ab21a3b1a8d2bd6b0364bd9c8059e9b
manav2217 <manavprajapati2217@gmail.com>
2023-06-06 22:33:50 +0530

    feat: about continue

--- src/Components/about/about.jsx
+++ src/Components/about/about.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import Me from "../../assets/me-about.jpg";
+import { BsAward } from "react-icons/bs";
+import { HiOutlineUserGroup } from "react-icons/hi";
+import { RiFolderSettingsLine } from "react-icons/ri";
+import './about.css'
 
 const about = () => {
   return (
-    <section id='about'>About</ section>
-  )
-}
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About me</h2>
 
-export default about
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={Me} alt="About Image" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <BsAward className="about_icon"/>
+              <h5>Experience</h5>
+              <small>2 Years Working</small>
+            </article>
+
+            <article className="about_card">
+              <HiOutlineUserGroup className="about_icon"/>
+              <h5>Clients</h5>
+              <small>0 as of now</small>
+            </article>
+
+            <article className="about_card">
+              <RiFolderSettingsLine className="about_icon"/>
+              <h5>Projects</h5>
+              <small>5 Personal Projects</small>
+            </article>
+          </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quos
+        repellendus fugit aspernatur quod sit sint eum! Voluptatum laborum optio
+        minima adipisci perferendis, reiciendis est facere, pariatur accusamus
+        accusantium recusandae alias vitae deleniti excepturi quas, sequi error
+        maxime vero ea asperiores quibusdam itaque. Fugit nesciunt facere
+        perferendis iusto, excepturi eaque!
+      </p>
+
+      <a className="btn btn-primary" href="#contact">Let's Talk</a>
+        </div>
+      </div>
+
+    </section>
+  );
+};
+
+export default about;
--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -1,21 +1,53 @@
-import React from 'react'
-import {AiOutlineHome} from "react-icons/ai"
-import {SiAboutdotme} from "react-icons/si"
-import {BiBookBookmark} from "react-icons/bi"
-import {MdHomeRepairService} from "react-icons/md"
-import {RiContactsLine} from "react-icons/ri"
-import './nav.css'
+import React, { useState } from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { SiAboutdotme } from "react-icons/si";
+import { BiBookBookmark } from "react-icons/bi";
+import { MdHomeRepairService } from "react-icons/md";
+import { RiContactsLine } from "react-icons/ri";
+import "./nav.css";
 
 const Nav = () => {
+  const [activeClass, setActiveClass] = useState("#");
+
   return (
     <nav>
-      <a href="#">{AiOutlineHome()}</a>
-      <a href="#about">{SiAboutdotme()}</a>
-      <a href="#experience">{BiBookBookmark()}</a>
-      <a href="#services">{MdHomeRepairService()}</a>
-      <a href="#contact">{RiContactsLine()}</a>
+      <a
+        href="#"
+        onClick={() => setActiveClass("#")}
+        className={activeClass === "#" ? "active" : ""}
+      >
+        {AiOutlineHome()}
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveClass("#about")}
+        className={activeClass === "#about" ? "active" : ""}
+      >
+        {SiAboutdotme()}
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveClass("#experience")}
+        className={activeClass === "#experience" ? "active" : ""}
+      >
+        {BiBookBookmark()}
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveClass("#services")}
+        className={activeClass === "#services" ? "active" : ""}
+      >
+        {MdHomeRepairService()}
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveClass("#contact")}
+        className={activeClass === "#contact" ? "active" : ""}
+      >
+        {RiContactsLine()}
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
